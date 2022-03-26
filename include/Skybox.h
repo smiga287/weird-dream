2a84ab427ef9b3271437a493331a381a489d2c9d
Aleksandar Smigic <smiga287@gmail.com>
2022-03-26 20:02:07 +0100

    Implement Skybox class

--- include/Skybox.h
+++ include/Skybox.h
@@ -5,26 +5,41 @@
 #ifndef PROJECT_BASE_SKYBOX_H
 #define PROJECT_BASE_SKYBOX_H
 
-
-#include "learnopengl/shader.h"
-#include "common.h"
 #include <vector>
 #include <string>
 #include <array>
+#include <span>
+#include <glm/glm.hpp>
+
+#include <learnopengl/shader.h>
 
 class Skybox {
 public:
-    void setup();
-    void load();
-    void render(const glm::mat4& view, const glm::mat4& projection);
-private:
-    uint loadCubemap(const std::vector<std::string>& faces);
+    explicit Skybox(const Shader& shader);
 
-    uint cubemap_texture_;
+    void loadTextures();
+    void render(const glm::mat4& view, const glm::mat4& projection);
+
+    // Given that we have a destructor, we should follow the rule of 5
+    ~Skybox();
+    // For now, we'll delete the rest of methods required by the rule of 5
+    Skybox(const Skybox& skybox) = delete;
+    Skybox(Skybox&& skybox) noexcept = delete;
+    Skybox& operator=(const Skybox& skybox) = delete;
+    Skybox& operator=(Skybox&& skybox) noexcept = delete;
+private:
+    void initOpenGL();
+    void deinitOpenGL();
+    uint loadCubemap(std::span<std::string> faces);
+
     Shader shader_;
+    uint cubemap_texture_;
+
+    uint skybox_VAO_;
+    uint skybox_VBO_;
 
     // skybox coordinates
-    constexpr static std::array skyboxVertices = {
+    constexpr static std::array skybox_vertices_ = {
             // positions
             -1.0f,  1.0f, -1.0f,
             -1.0f, -1.0f, -1.0f,
