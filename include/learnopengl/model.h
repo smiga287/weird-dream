20146e4189fcdb10c041ce6a153bfd100c8e1209
Aleksandar Smigic <smiga287@gmail.com>
2022-03-26 20:04:54 +0100

    Fix initialization warnings

--- include/learnopengl/model.h
+++ include/learnopengl/model.h
@@ -229,14 +229,13 @@ unsigned int TextureFromFile(const char *path, const string &directory, bool gam
     unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
     if (data)
     {
-        GLenum format;
-        if (nrComponents == 1)
-            format = GL_RED;
-        else if (nrComponents == 3)
-            format = GL_RGB;
-        else if (nrComponents == 4)
-            format = GL_RGBA;
-
+        auto format = [nrComponents]() -> GLint {
+            switch (nrComponents) {
+                case 1: return GL_RED;
+                case 3: return GL_RGB;
+                default: return GL_RGBA;
+            }
+        }();
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
