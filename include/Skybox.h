a0bcb6a34f39621372cdb1e4df659f79c0d5afc2
Aleksandar Smigic <smiga287@gmail.com>
2022-03-26 19:59:47 +0100

    Create files for Skybox class

---
+++ include/Skybox.h
@@ -0,0 +1,74 @@
+//
+// Created by smiga287 on 3/26/22.
+//
+
+#ifndef PROJECT_BASE_SKYBOX_H
+#define PROJECT_BASE_SKYBOX_H
+
+
+#include "learnopengl/shader.h"
+#include "common.h"
+#include <vector>
+#include <string>
+#include <array>
+
+class Skybox {
+public:
+    void setup();
+    void load();
+    void render(const glm::mat4& view, const glm::mat4& projection);
+private:
+    uint loadCubemap(const std::vector<std::string>& faces);
+
+    uint cubemap_texture_;
+    Shader shader_;
+
+    // skybox coordinates
+    constexpr static std::array skyboxVertices = {
+            // positions
+            -1.0f,  1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            -1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f
+    };
+};
+
+
+#endif //PROJECT_BASE_SKYBOX_H
---
+++ src/Skybox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by smiga287 on 3/26/22.
+//
+
+#include "Skybox.h"
