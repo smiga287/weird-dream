7adb449230ef9caf4fe1430419f82fdcfd8c4283
Aleksandar Smigic <smiga287@gmail.com>
2022-04-10 01:08:19 +0200

    Change screen resolution

--- include/drawing.h
+++ include/drawing.h
@@ -6,7 +6,7 @@ class Shader;
 class Floor;
 class Benders;
 
-void setup_lighting(const Shader& lights_shader, const ProgramState& state);
+void setup_lighting(const Shader &lights_shader, const Shader &lights_box_shader, const ProgramState &state);
 void draw_skybox(const Skybox& skybox, const ProgramState& state);
 void draw_floor(const Floor& floor, const ProgramState& state);
 void draw_building(const Model& building_model, const Shader& shader, const ProgramState& state);
